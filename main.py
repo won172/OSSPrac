9301ae58ec1fbe6d86baa5f5ba425089d0c50903
won172 <swc3466@naver.com>
2025-03-31 15:45:44 +0900

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print('Woncheol Shin')
+print('hello world')  
+name='WCShin'  
+print(f'I\'m WC Shin!')  
+programming_language='Python'  
+print('Most used language is {0}'.format(programming_language))
