18143ef68a91b495cb89de6cd584aceaa727cb64
won172 <swc3466@naver.com>
2025-04-04 02:52:56 +0900

    grade

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 name = input('이름을 입력하세요: ')
 num = input('학번을 입력하세요: ')
 depart = input('학과를 입력하세요: ')
+grade = input('학년을 입력하세요: ')
 
 print('<출력>')
 print('이름:', name)
 print('학번:', num)
 print('학과:', depart)
+print('학년:', grade)
