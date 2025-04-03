d8a4c9a5268c93cecbb20128ae86e93dcd353159
won172 <swc3466@naver.com>
2025-04-04 02:46:13 +0900

    univ

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 name = input('이름을 입력하세요: ')
 num = input('학번을 입력하세요: ')
 depart = input('학과를 입력하세요: ')
+univ = input('학교를 입력하세요: ')
 
 print('<출력>')
 print('이름:', name)
 print('학번:', num)
 print('학과:', depart)
+print('학교:', univ)
